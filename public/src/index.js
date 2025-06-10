602f2a9399ab19dc7c31e4102591974c3539906b
emreakka <133929593+emreakka@users.noreply.github.com>
2025-06-10 12:28:14 +0300

    Create index.js

---
+++ public/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AWLVocabularyApp from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AWLVocabularyApp />);
